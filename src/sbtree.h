3a6b16747b7583f19e25a8ecb1f36d2540d1ea58
Ramon Lawrence <ramon.lawrence@ubc.ca>
2021-03-24 13:59:32 -0700

    Remove unneeded declaration

--- src/sbtree.h
+++ src/sbtree.h
@@ -71,17 +71,6 @@ typedef uint16_t count_t;
 #define SBTREE_SET_INTERIOR(x) 	SBTREE_SET_COUNT(x,*((count_t *) (x+SBTREE_COUNT_OFFSET))+10000)
 #define SBTREE_SET_ROOT(x) 		SBTREE_SET_COUNT(x,*((count_t *) (x+SBTREE_COUNT_OFFSET))+20000)
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0') 
-
 #define MAX_LEVEL 8
 
 typedef struct {			
